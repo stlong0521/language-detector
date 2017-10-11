7353ca06e919300b8200595002141607eaffe2b4
Tianlong Song <stlong0521@gmail.com>
2017-10-10 23:50:36 -0700

    Enable predicting new images by model

--- language_detector/modeling/predict.py
+++ language_detector/modeling/predict.py
@@ -1,23 +1,27 @@
-import tensorflow as tf
 import numpy as np
 import yaml
-from scipy.ndimage import imread
-from network.instances.berlinnet import net
 import networkinput
 import argparse
+import importlib
 
-config = yaml.load(file("config.yaml"))
+def predict(args):
+    config = yaml.load(file(args.config))
 
-def predict(image_path, model_path):
-    image = networkinput.read_png(image_path, "L")
-    net.predict(model_path, np.expand_dims(image, axis=0))
+    network_module = "network.instances.{0}".format(config["NET"])
+    network_generator = getattr(importlib.import_module(network_module), "generate")
+
+    net = network_generator(config["INPUT_SHAPE"], config["OUTPUT_SHAPE"][0])
+
+    image = networkinput.read_png(args.image_path, "L")
+    net.predict(args.model_path, np.expand_dims(image, axis=0))
 
 if __name__ == "__main__":  
 
     parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", default="config.yaml", help="Path to config file")
     parser.add_argument('--image', dest='image_path', help='Path to image file', required=True)
     parser.add_argument('--model', dest='model_path', required=True, help='Path to saved tensorflow model')
 
     args = parser.parse_args()
 
-    predict(args.image_path, args.model_path)
+    predict(args)
--- ubuild.py
+++ ubuild.py
@@ -1,15 +1,12 @@
 import os
 import sys
 
-def main(build):
-    build.executables.run([sys.executable, "language_detector/main.py"])
-
-def download_raw_data(build):
+def download(build):
     build.packages.install("pyyaml", version="==3.12")
     build.packages.install("youtube-dl", version="==2017.9.24")
     build.executables.run([sys.executable, "language_detector/data_acquisition/download.py"])
 
-def preprocessing(build):
+def preprocess(build):
     os.environ['SPARK_HOME'] = '/usr/local/share/spark/spark-2.0.2'
     build.packages.install("opencv-python", version="==3.3.0.10")
     build.packages.install("matplotlib", version="==1.5.0")
@@ -27,14 +24,14 @@ def preprocessing(build):
         "--input-path", "data/rst",
         "--output-path", "data/labelled"])
 
-def train_model(build):
+def train(build):
     build.packages.install("tensorflow", version="==1.3.0")
     build.packages.install("pillow", version="==4.3.0")
     build.executables.run([
         sys.executable, "language_detector/modeling/train.py",
         "--config", "language_detector/modeling/config.yaml"])
 
-def evaluate_model(build):
+def evaluate(build):
     build.packages.install("tensorflow", version="==1.3.0")
     build.packages.install("pillow", version="==4.3.0")
     build.packages.install("pyyaml", version="==3.12")
@@ -43,4 +40,16 @@ def evaluate_model(build):
     build.executables.run([
         sys.executable, "language_detector/modeling/evaluate.py",
         "--config", "language_detector/modeling/config.yaml",
+        "--model", checkpoint['model_checkpoint_path']])
+
+def predict(build):
+    build.packages.install("tensorflow", version="==1.3.0")
+    build.packages.install("pillow", version="==4.3.0")
+    build.packages.install("pyyaml", version="==3.12")
+    import yaml
+    checkpoint = yaml.load(file("snapshots/Berlin_2017-10-10T00-00-56/checkpoint"))
+    build.executables.run([
+        sys.executable, "language_detector/modeling/predict.py",
+        "--config", "language_detector/modeling/config.yaml",
+        "--image", "data/rst/chinese/ 20130207_00.png",
         "--model", checkpoint['model_checkpoint_path']])
